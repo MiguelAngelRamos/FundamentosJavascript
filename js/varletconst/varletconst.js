71229e5357ee2b3bd4ac9a54c3973ee1f84fff5f
Miguel Ramos <return.infox@gmail.com>
2022-01-06 00:37:01 -0300

    Fundamentos de Javascript curso de FrontEnd

---
+++ js/varletconst/varletconst.js
@@ -0,0 +1,17 @@
+// var, let y const
+/* En este archivo
+vamos aprender los variables */
+
+var edad = 30;
+var edad = 40;
+/* Problema: estamos declarando 2 veces la misma variable y le estamos cambiando el valor */
+console.log(edad);
+
+let altura = 150; // declarando por que usamos let
+altura = 200; // aca estamos cambiando su valor y no declarando
+console.log(altura);
+
+// const "Constante"
+const velocidad = 100;
+velocidad = 400;
+console.log(velocidad);
